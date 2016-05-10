2ec36d7afd4b8815070aec15671eaa398d161e73
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-05-10 17:34:59 -0400

    new stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += %w( modernizr.custom )
 Rails.application.config.assets.precompile += %w( default.css )
 Rails.application.config.assets.precompile += %w( component.css )
 
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
