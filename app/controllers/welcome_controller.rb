c2dd6287f9e1709585cbb0fbaa046d22505f34dd
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-05-10 18:52:43 -0400

    roles added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
+  before_action :authorize_user, except: [:index, :show]
+
   def index
     render :index
+  end
+
+  def authorize_user
+    if !user_signed_in? || !current_user.admin?
+      raise ActionController::RoutingError.new("Not Found")
+    end
   end
 end
