d9c91408d0e18185d2c40daf0577bf9030b222e2
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-05-18 12:58:41 -0400

    everything but tweets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( bootstrap-responsive.css )
 Rails.application.config.assets.precompile += %w( grid.css )
+Rails.application.config.assets.precompile += %w( foot.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
