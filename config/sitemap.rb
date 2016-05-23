cfd3acefea1cca47953dbe86d669d336053ee8a3
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-05-23 18:58:35 -0400

    finally

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,4 @@
-# Set the host name for URL creation
+
 SitemapGenerator::Sitemap.default_host = "http://www.doctorloans.io"
 
 SitemapGenerator::Sitemap.create do
@@ -24,16 +24,11 @@ SitemapGenerator::Sitemap.create do
   #   Article.find_each do |article|
   #     add article_path(article), :lastmod => article.updated_at
   #   end
-  Apply.find_each do |apply|
-    add apply_path(apply), :lastmod => apply.updated_at
-  end
-  Contact.find_each do |contact|
-    add contact_path(contact), :lastmod => contact.updated_at
-  end
-  Loan.find_each do |loan|
-    add loan_path(loan), :lastmod => loan.updated_at
-  end
-  Testimonial.find_each do |testimonial|
-    add testimonial_path(testimonial), :lastmod => testimonial.updated_at
-  end
+
+add new_apply_path, :priority => 0.7, :changefreq => 'daily'
+
+add doctor_path, :priority => 0.7, :changefreq => 'daily'
+add other_path, :priority => 0.7, :changefreq => 'daily'
+
+
 end
