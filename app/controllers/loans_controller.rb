f669b93bad6d1892a5c4ef601758269df61bcf8c
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-05-24 18:17:50 -0400

    meta_tags

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -2,12 +2,18 @@ class LoansController < ApplicationController
 
 
   def doctor
+    set_meta_tags title: 'DoctorLoans.io:  Doctor Loan Guidelines',
+              description: 'The DoctorLoans.io Doctor Loan Page',
+              keywords: 'physician loans, physician loan, physician personal loans, loans for physicians, resident physician loans, physician loan program, loans for resident physicians, personal loans for physicians, regions bank physician loan, physician loan rates, business loans for physicians,exclusive physician loans, physician loan interest rate, physician loans rates, physician relocation loan, physician practice loans, home loans for dentists, mortgages for dentists, dentist mortgage'
     render :doctor
     # @user_rating = @testimonials.average(:rating).to_i
   end
 
 
   def other
+    set_meta_tags title: 'DoctorLoans.io:  Other Loan Guidelines',
+              description: 'The DoctorLoans.io Other Loan Options Page',
+              keywords: 'mortgage, refinance, purchase a home, FHA, conforming, Jumbo, mortgage rates'
     render :other
     # @user_rating = @testimonials.average(:rating).to_i
   end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,9 @@ class WelcomeController < ApplicationController
   before_action :authorize_user, except: [:index, :show]
 
   def index
-
+    set_meta_tags title: 'DoctorLoans.io:  The home mortgage resource for Doctors',
+              description: 'The DoctorLoans.io Home Page',
+              keywords: 'physician loans, physician loan, physician personal loans, loans for physicians, resident physician loans, physician loan program, loans for resident physicians, personal loans for physicians, regions bank physician loan, physician loan rates, business loans for physicians,exclusive physician loans, physician loan interest rate, physician loans rates, physician relocation loan, physician practice loans, home loans for dentists, mortgages for dentists, dentist mortgage'
     @testimonials = Testimonial.all
     render :index
     # @user_rating = @testimonials.average(:rating).to_i
