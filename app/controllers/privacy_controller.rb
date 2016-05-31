e165bfb767b1c89a99f0a7b040d127bdf348849c
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-05-31 12:13:32 -0400

    done

---
+++ app/controllers/privacy_controller.rb
@@ -0,0 +1,13 @@
+class PrivacyController < ApplicationController
+
+
+  def index
+    set_meta_tags title: 'Privacy Policy and Disclaimer - DoctorLoans.io',
+              description: 'DoctorLoans.io:  Site Privacy Policy and Disclaimer',
+              keywords: 'physician loans, physician loan, physician personal loans, loans for physicians, resident physician loans, physician loan program, loans for resident physicians, personal loans for physicians, regions bank physician loan, physician loan rates, business loans for physicians,exclusive physician loans, physician loan interest rate, physician loans rates, physician relocation loan, physician practice loans, home loans for dentists, mortgages for dentists, dentist mortgage'
+  end
+end
+
+
+
+
