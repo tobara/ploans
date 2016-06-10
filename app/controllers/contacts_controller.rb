4f7b93dd8a2fae8344e1f851c128502b20e90bc8
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-06-10 11:04:45 -0400

    meta updates

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,6 +13,9 @@ class ContactsController < ApplicationController
 
   def new
     add_breadcrumb "Contact Us"
+    set_meta_tags title: 'DoctorLoans.io:  Contact Us.  Mortgage questions?  Inquire here.',
+              description: 'The DoctorLoansIO Contact Page',
+              keywords: 'physician loans, physician loan, physician personal loans, loans for physicians, resident physician loans, physician loan program, loans for resident physicians, personal loans for physicians, regions bank physician loan, physician loan rates, business loans for physicians,exclusive physician loans, physician loan interest rate, physician loans rates, physician relocation loan, physician practice loans, home loans for dentists, mortgages for dentists, dentist mortgage'
     @contact = Contact.new
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   before_action :authorize_user, except: [:index, :show]
 
   def index
-    set_meta_tags title: 'DoctorLoans.io:  The home mortgage resource for Doctors',
-              description: 'The DoctorLoans.io Home Page',
+    set_meta_tags title: 'DoctorLoans.io:  The Home Loan Resource for Physicians',
+              description: 'The DoctorLoansIO Home Page',
               keywords: 'physician loans, physician loan, physician personal loans, loans for physicians, resident physician loans, physician loan program, loans for resident physicians, personal loans for physicians, regions bank physician loan, physician loan rates, business loans for physicians,exclusive physician loans, physician loan interest rate, physician loans rates, physician relocation loan, physician practice loans, home loans for dentists, mortgages for dentists, dentist mortgage'
     @testimonials = Testimonial.all
     render :index
