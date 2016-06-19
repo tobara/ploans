20421f0804bad6d33b1dc93da6965df88d5304a9
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-06-19 10:41:12 -0400

    dynamic routing for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   add_breadcrumb "Home", :root_path, :title => "Back to Doctor Loans IO Home Page"
 
   def show
+    @article = Article.find_by_title(params[:article])
     @article = Article.find(params[:id])
     add_breadcrumb "Mortgage Insights: #{@article.title}"
     set_meta_tags title: "Doctor Loans IO - Mortgage Insights: #{@article.title}"
